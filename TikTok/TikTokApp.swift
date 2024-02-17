76aa48f562236ee89b03bcc9a2ec021ac9831001
ShivShah202000 <shivshah20112000@gmail.com>
2024-02-17 14:18:45 -0800

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by dtas-dev on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
